68291a90df41a54179abed3c905401df82d62173
Orion Gregg-Schofield <noiroelite@windowslive.com>
2015-06-12 15:29:31 +1200

    updated DB

--- assets/cheapo.sql
+++ assets/cheapo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 11, 2015 at 02:32 am
+-- Generation Time: Jun 12, 2015 at 05:25 am
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -23,6 +23,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+`ID` mediumint(8) unsigned NOT NULL,
+  `Username` varchar(20) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(60) NOT NULL,
+  `Privilege` enum('admin','moderator','user') NOT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`ID`, `Username`, `Email`, `Password`, `Privilege`, `CreationDate`) VALUES
+(1, 'admin', 'admin@admin.com', 'admin', 'admin', '2015-06-12 02:47:00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pages`
 --
 
@@ -31,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `pages` (
   `Title` varchar(60) NOT NULL,
   `Description` varchar(160) NOT NULL,
   `Name` varchar(20) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `pages`
@@ -41,27 +63,39 @@ INSERT INTO `pages` (`ID`, `Title`, `Description`, `Name`) VALUES
 (1, 'Cheap deals for students - Home', 'The best student deals in NZ since ages ago', 'home'),
 (2, 'Cheap deals for students - About Us', 'All the info you could ever need to know about us and what we do', 'about'),
 (3, 'Cheap deals for students - Contact Us', 'Contact us info to harass us to fix or improve something', 'contact'),
-(4, 'Cheap deals for students - Page not found, try again', 'OOPS, sorry we could not find the page you were looking for', '404');
+(4, 'Cheap deals for students - Page not found, try again', 'OOPS, sorry we could not find the page you were looking for', '404'),
+(5, 'Cheap deals for students - Register', 'Register a new account to get access to cheap student deals all over the country', 'register');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+ ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `pages`
 --
 ALTER TABLE `pages`
- ADD PRIMARY KEY (`ID`);
+ ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `Name` (`Name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+MODIFY `ID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `pages`
 --
 ALTER TABLE `pages`
-MODIFY `ID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `ID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
